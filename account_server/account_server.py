4eb675205cb7280314e26818e75d6ac5f5000389
CGandGameEngineLearner <lifesize1@qq.com>
2025-03-19 13:55:34 +0800

    准备合并

--- account_server/account_server.py
+++ account_server/account_server.py
@@ -1,11 +1,24 @@
 import asyncio
 import grpc
 import proto
+from config import config
+from loguru import logger
 
+logger.add(
+    sink=config['Log']['sink'],
+    rotation=config['Log']['rotation'],
+    retention=config['Log']['retention'],
+    compression=config['Log']['compression'],
+    enqueue=config['Log']['enqueue'],
+    backtrace=config['Log']['backtrace'],
+    diagnose=config['Log']['diagnose'],
+    level=config['Log']['level'],
+)
 
 class account_server:
     def __init__(self, host: str, port: int):
         self.host = host
         self.port = port
 
-        
+    def run(self):
+        pass
---
+++ account_server/account_server_rpc_servicer.py
@@ -0,0 +1,4 @@
+
+
+class AccountServerRpcServicer:
+    pass
